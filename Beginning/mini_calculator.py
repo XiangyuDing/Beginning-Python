63af3565e5598515d17dafa48db54f7be9455bf8
Xiangyu Ding <xid30@pitt.edu>
2018-01-18 19:48:23 -0500

    mini-program
    some test code

---
+++ Beginning/mini_calculator.py
@@ -0,0 +1,31 @@
+import sys # sys.exit() quits the program
+
+line = input()
+split = line.split()
+
+if len(split) != 3:
+    print("Wrong format!")
+    sys.exit()
+
+left = int(split[0])
+op = split[1]
+right = int(split[2])
+
+val = 0
+
+if op == '+':
+    val = left + right
+elif op == '-':
+    val = left - right
+elif op == '*':
+    val = left * right
+elif op == '/':
+    if right == 0:
+        print("Division by zero. Halting")
+        sys.exit()
+    val = left / right
+else:
+    print("Unknown operator:{operator}".format(operator=op))
+    sys.exit()
+
+print("{line_expr} = {value:.2f}".format(line_expr=line, value=val))
